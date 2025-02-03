7e6310a29c1a6ab0e8961aed0a73d8d1f3d8ca4a
marina-medeiros <marinamaleite@gmail.com>
2025-02-03 09:19:43 -0300

    quick fix

---
+++ build/CMakeFiles/sloc.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sloc.dir/src/classify.cpp.o"
+  "CMakeFiles/sloc.dir/src/classify.cpp.o.d"
+  "CMakeFiles/sloc.dir/src/directoryManager.cpp.o"
+  "CMakeFiles/sloc.dir/src/directoryManager.cpp.o.d"
+  "CMakeFiles/sloc.dir/src/input.cpp.o"
+  "CMakeFiles/sloc.dir/src/input.cpp.o.d"
+  "CMakeFiles/sloc.dir/src/main.cpp.o"
+  "CMakeFiles/sloc.dir/src/main.cpp.o.d"
+  "CMakeFiles/sloc.dir/src/printing.cpp.o"
+  "CMakeFiles/sloc.dir/src/printing.cpp.o.d"
+  "sloc"
+  "sloc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sloc.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sloc.
+# This may be replaced when dependencies are built.
